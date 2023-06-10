0847507b895afdf2ded0593a1f0dd9fe64850788
PythonicHiss <135261945+PythonicHiss@users.noreply.github.com>
2023-06-10 22:33:54 +0100

    Update EulerProject#1.py
    Went back to make better solution

--- EulerProject#1.py
+++ EulerProject#1.py
@@ -1,29 +1,12 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
+#Find the sum of all the mutiples of 3 or 5 below 1000.
+#Correct answer is 233168
 
-def ListNat():
-    """Generates a list of numbers divisible by two given numbers and calculates their sum."""
-    Nat = []
-    print("Please select the limit:")
-    n = int(input())
-    #Should learn how to test values
-    print("Thank you, now please input two numbers that you want to find divisible numbers of (up-to the limit):")
-    m = int(input())
-    k = int(input())
-    print(f'You chose {m} and {k}, if you wish to change your choice please input "CHANGE", if not, please enter "GO".')
-    p = str(input())
-    if p == "CHANGE":
-        print("Please input the desired two numbers")
-        m = int(input())
-        k = int(input())
-        if p == "GO":
-            pass
-    for num in range(0, int(n)):
-        if num%m == 0 or num%k == 0:
-            Nat.append(num)
-    if n%m == 0 or n%k == 0:
-        Nat.append(n)
-        pass
-    print(f'The numbers from 0 to {n} that are divisible by {m} and {k} are: {Nat}')
-    print(f'The sum of all of these numbers is {sum(Nat)}')
-    return
-
-ListNat()
+def Divisible(n):
+    """Basic solution"""
+    Numbers = 0
+    for i in range(1,n):
+        if i%3==0 or i%5==0:
+            Numbers += i
+    return print(Numbers)
+Divisible(1000)
